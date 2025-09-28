f58ffbef533cccfb962fdb9d080605413074c412
Ámir Orman <amirtmb@MacBook-Air-Amir.local>
2025-09-28 23:55:40 +0500

    add Closest Pair of Points with tests to testing by jup

--- src/test/java/org/algoritms/geometry/ClosestPairTest.java
+++ src/test/java/org/algoritms/geometry/ClosestPairTest.java
@@ -1,4 +1,25 @@
 package org.algoritms.geometry;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ClosestPairTest {
+
+    @Test
+    void testClosestPair() {
+        ClosestPair.Point[] points = {
+                new ClosestPair.Point(2, 3),
+                new ClosestPair.Point(12, 30),
+                new ClosestPair.Point(40, 50),
+                new ClosestPair.Point(5, 1),
+                new ClosestPair.Point(12, 10),
+                new ClosestPair.Point(3, 4)
+        };
+
+        ClosestPair cp = new ClosestPair();
+        double result = cp.closest(points);
+
+        assertEquals(Math.sqrt(2), result, 1e-6);
+        System.out.println("Closest distance: " + result);
+    }
 }
